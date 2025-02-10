409ef74a92a634b7829f65732461d7273c0bf60d
sabashaikh4 <sabahshaikh361@gmail.com>
2025-02-10 21:02:09 +0530

    Initial commit

--- WEBSITE/src/main/java/com/MUSH_FOOD/model/IngredientsCategory.java
+++ WEBSITE/src/main/java/com/MUSH_FOOD/model/IngredientsCategory.java
@@ -1,33 +1,32 @@
 package com.MUSH_FOOD.model;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class IngredientsItem {
+
+public class IngredientsCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private  Long id;
+    private Long id;
 
     private String name;
 
-
-    @ManyToOne // many ingre have same category
-    private IngredientsCategory category;
-
-
     @JsonIgnore
-    @ManyToOne // one restau have multiple ingre
-    private Restaurant restaurant ;
+    @ManyToOne
+    private Restaurant restaurant;
 
-    private boolean inStoke = true;
 
+    @OneToMany(mappedBy = "category" ,cascade = CascadeType.ALL)
+    private List<IngredientsItem> ingredients = new ArrayList<>();
 }
